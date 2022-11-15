54d3fff4769d888fd8bcd3ac1b75de422101b37d
YoTi <yasserrafai5@gmail.com>
2022-11-15 22:39:37 +0100

    server done

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import scoket
 import threading
 
 import rsa
+
+
+choice = input("Do you want to Host (1), or to connect (2): ")
+
+if choice == '1':
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("192.168.1.6", 4444))
+    server.listen()
