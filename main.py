ddf38d47256e193999149c86fba87968871212da
YoTi <yasserrafai5@gmail.com>
2022-11-15 23:26:00 +0100

    chat work perfectly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import scoket
+import socket
 import threading
 
 import rsa
@@ -8,12 +8,27 @@ choice = input("Do you want to Host (1), or to connect (2): ")
 
 if choice == '1':
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(("192.168.1.6", 4444))
+    server.bind(("192.168.1.6", 9999))
     server.listen()
 
     client, _ = server.accept()
 elif choice == '2':
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect(("192.168.1.6"))
+    client.connect(("192.168.1.6", 9999))
 else:
     exit()
+
+
+def sending_messages(c):
+    while True:
+        message = input("")
+        c.send(message.encode())
+
+
+def receiving_messages(c):
+    while True:
+        print("Partner: " + c.recv(1024).decode())
+
+
+threading.Thread(target=sending_messages, args=(client,)).start()
+threading.Thread(target=receiving_messages, args=(client,)).start()
