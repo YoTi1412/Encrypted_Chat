c5486ee655235d0b7f8c7253ecb2b6a1c7f75545
YoTi <yasserrafai5@gmail.com>
2022-11-16 01:10:10 +0100

    Done

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import threading
 
 import rsa
 
+public_key, private_key = rsa.newkeys(1024)
+public_partner = None
+
 
 choice = input("Do you want to Host (1), or to connect (2): ")
 
@@ -12,9 +15,14 @@ if choice == '1':
     server.listen()
 
     client, _ = server.accept()
+    client.send(public_key.save_pkcs1("PEM"))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
 elif choice == '2':
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(("192.168.1.6", 9999))
+    public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+    client.send(public_key.save_pkcs1("PEM"))
+
 else:
     exit()
 
@@ -22,12 +30,13 @@ else:
 def sending_messages(c):
     while True:
         message = input("")
-        c.send(message.encode())
+        c.send(rsa.encrypt(message.encode(), public_partner))
+        print("You: " + message)
 
 
 def receiving_messages(c):
     while True:
-        print("Partner: " + c.recv(1024).decode())
+        print("Partner: " + rsa.decrypt(c.recv(1024), private_key).decode())
 
 
 threading.Thread(target=sending_messages, args=(client,)).start()
